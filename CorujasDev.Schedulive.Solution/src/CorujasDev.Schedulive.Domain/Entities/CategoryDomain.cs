e67eb7fbb22b3f71ab1b11e98ffa5918d6131894
corujasdevbr <fernando.guerra@corujasdev.com.br>
2020-05-03 03:37:13 -0300

    Criação classe Category e Interface no projeto Domain

---
+++ CorujasDev.Schedulive.Solution/src/CorujasDev.Schedulive.Domain/Entities/CategoryDomain.cs
@@ -0,0 +1,22 @@
+﻿using CorujasDev.Schedulive.Core.DomainObjects;
+using Flunt.Validations;
+
+namespace CorujasDev.Schedulive.Domain.Entities
+{
+    public class CategoryDomain : Entity
+    {
+        public string Name { get; private set; }
+
+        public CategoriesDomain(string name)
+        {
+            AddNotifications(new Contract()
+                .IsNotNullOrEmpty(name, "Firstname", "FirstName is required")
+            );
+
+            if (Valid)
+            {
+                Name = name;
+            }
+        }
+    }
+}
---
+++ CorujasDev.Schedulive.Solution/src/CorujasDev.Schedulive.Domain/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using CorujasDev.Schedulive.Domain.Entities;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CorujasDev.Schedulive.Domain.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        #region Read
+        IQueryable<CategoryDomain> GetAll();
+        CategoryDomain GetById(Guid id);
+        CategoryDomain GetByName(string name);
+        IQueryable<CategoryDomain> FindBy(Expression<Func<CategoryDomain, bool>> predicate);
+        #endregion
+
+        #region Write
+        void Add(CategoryDomain Category);
+        void Update(CategoryDomain Category);
+        #endregion
+    }
+}
